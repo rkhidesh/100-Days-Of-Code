3ab0bd37ae65ba459dc4fb951f18d90258be74b8
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-05-17 13:40:14 -0400

    packages

---
+++ 100 Days/day16.py
@@ -0,0 +1,22 @@
+# from turtle import Turtle, Screen
+# import turtle
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("chartreuse4")
+# timmy.forward(100)
+# timmy.left(90)
+
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
