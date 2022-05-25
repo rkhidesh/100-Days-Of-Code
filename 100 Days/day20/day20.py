777c6c288e09bf125a113ed17d478134096c2b18
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-05-25 13:11:20 -0400

    modified

--- 100 Days/day20/day20.py
+++ 100 Days/day20/day20.py
@@ -34,14 +34,13 @@ while game_is_on:
         score.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
 
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
-
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            score.reset()
 
 screen.exitonclick()
 
--- 100 Days/day20/scoreboard.py
+++ 100 Days/day20/scoreboard.py
@@ -5,6 +5,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -13,14 +14,16 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Courier", 14, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High score {self.high_score}",align="center", font=("Courier", 14, "normal"))
        
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
-
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"Game Over", align="center", font=("Courier", 14, "normal"))
-
+    
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+ 
