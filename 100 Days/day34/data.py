11e459e61777fc98db6dede63ce45338d6dd136e
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-06-07 13:41:15 -0400

    day34

---
+++ 100 Days/day34/data.py
@@ -0,0 +1,8 @@
+from multiprocessing import connection
+import requests
+
+response = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
+
