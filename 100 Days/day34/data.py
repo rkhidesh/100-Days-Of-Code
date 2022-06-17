7a36b276e0f7f1f42ba5590019ae230e5137f23c
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-06-17 15:43:38 -0400

    day 45

--- 100 Days/day34/data.py
+++ 100 Days/day34/data.py
@@ -5,4 +5,4 @@ response = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean"
 response.raise_for_status()
 data = response.json()
 question_data = data["results"]
-
+print(question_data)
